51c64e5423fcf89359040a438d1a301ef5240ccd
vanu888 <173596364+vanu888@users.noreply.github.com>
2026-03-04 13:39:47 +0530

    Add additional tools

--- setup.sh
+++ setup.sh
@@ -57,3 +57,32 @@ echo "🛠️ Installing additional utilities..."
 sudo apt install -y htop neofetch unzip wget 
 
 echo "✅ All tools installed successfully!"
+
+echo -e " \n
+Additional tools you can use to set up your device:\n
+    * Firmware Update : 'sudo apt install linux-firmware'\n
+    * To check mousepoint installed : 'sudo apt install server-xorg-input-libinput' 
+                            or 'sudo apt install xserver-xrog-input-synaptics'\n
+    * Battery (power management) : 'sudo apt install tlp'\n
+    * Power management : 'sudo systemctl enable tlp'\n
+    * Enable tlp : 'sudo systemctl start tlp'\n
+    * Check tlp status : 'sudo tlp-stat -s'\n
+    * Tool for battery monitoring : 'sudo apt install acpi'\n
+    * Check battery status : 'acpi -V'\n
+    * Monitor temp and fan info : 'sudo apt install lm-sensors'\n
+    * Indicator temp and RAM : 'sudo apt install indicator-multiload'\n
+    * Touchpad and config : 'sudo apt install xinput'\n
+    * Bluetooth GUI : 'sudo apt install blueman'\n
+    * Core bluetooth stack : 'sudo apt install blucz'\n
+    * Firmware update tool : 'sudo apt install fwupd'\n
+    * Check firmware update running : 'fwupdmgr get-updates'\n
+    * CPU frequency scaling : 'sudo apt install cpu-frequtils'\n
+    * Tune power : 'sudo apt install powertop'\n
+    * Manage and perform HD : 'sudo apt install hdparm'\n
+    * Multimedia (GPU) : 'sudo apt install mesa-utils'\n
+    * Non-free firmware : 'sudo apt install firmware-misc-nonfree'\n
+    * System cleaner : 'sudo apt install bleachbit'\n
+    * For Thinkpad users :\n 
+        * Power management : 'sudo modprobe thinkpad_acpi'\n
+        * Fan controll tool : 'sudo apt install thinkfan'
+    "
